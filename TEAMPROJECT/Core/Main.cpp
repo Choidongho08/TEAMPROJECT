5d9968a92d2507b205432d8176dbac7f1981c691
kim-jinho1 <russiakim2008@gmail.com>
2025-06-09 12:43:50 +0900

    feat : mainLogic

--- TEAMPROJECT/Core/Main.cpp
+++ TEAMPROJECT/Core/Main.cpp
@@ -1,7 +1,31 @@
-#include "Core.h"
+#include "../DH/GameLogic.h"
+#include "../DH/TitleScene.h"
+#include "../DH/AsciiObject.h"
+#include "Mci.h"
+#include<iostream>
 
 int main()
 {
-	Core core;
-	core.Run();
+	Scene curScene = Scene::TITLE;
+	AsciiObjcets objs;
+	char gameMap[MAP_HEIGHT][MAP_WIDTH] = {};
+	PLAYER player = {};
+	vector<BOMB> vecBomb;
+	Init(objs, gameMap, &player);
+	while (curScene != Scene::QUIT)
+	{
+		switch (curScene)
+		{
+		case Scene::TITLE:
+			TitleScene(curScene, objs);
+			break;
+		case Scene::GAME:
+			GameScene(curScene, gameMap, &player, vecBomb);
+			break;
+		case Scene::INFO:
+			InfoScene(curScene);
+			break;
+		}
+	}
+	ReleaseAllSounds();
 }
