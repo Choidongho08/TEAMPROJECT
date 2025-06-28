669d3a1fc35a4c2834f8709a79aeda9c95154cce
kim-jinho1 <russiakim2008@gmail.com>
2025-06-28 11:40:15 +0900

    fix : info, result scene ui pos

--- TEAMPROJECT/JH/CPP/ResultScene.cpp
+++ TEAMPROJECT/JH/CPP/ResultScene.cpp
@@ -17,17 +17,8 @@ ResultScene::~ResultScene()
 void ResultScene::SceneInit(SCENE _type, std::vector<AsciiObject>* _asciiObjects)
 {
     system("cls");
-    COORD resolution = GetConsoleResolution();
-    int x = (resolution.X / 2.1);
-    int y = (resolution.Y / 6) + 10;
-
-    GotoXY(x - 4, y);
-    cout << "  [";
-    GotoXY(x + 10, y);
-    cout << "]  ← 스페이스바로 선택";
 
     Render(1);
-    RenderClear();
     if (_type == SCENE::DEAD) {
         RenderGameOver();
     }
@@ -38,67 +29,120 @@ void ResultScene::SceneInit(SCENE _type, std::vector<AsciiObject>* _asciiObjects
 
 Menu ResultScene::GetCurMenu()
 {
-    COORD resolution = GetConsoleResolution();
-    int x = (resolution.X / 2.1);
-    static int y = (resolution.Y / 6) + 10;
-    static int originY = y;
-    Key eKey = KeyController();
-    switch (eKey)
-    {
-    case Key::UP:
-        PlaySoundID(SOUNDID::UPDOWN);
-        if (y > originY)
-        {
-            GotoXY(x - 5, y);
-            cout << "    ";
-            GotoXY(x + 10, y);
-            cout << "                       ";
-            GotoXY(x - 4, --y);
-            cout << "  [";
-            GotoXY(x + 10, y);
-            cout << "]  ← 스페이스바로 선택";
-            Sleep(100);
-        }
-        break;
-    case Key::DOWN:
-        PlaySoundID(SOUNDID::UPDOWN);
-        if (y < originY + 2)
-        {
-            GotoXY(x - 5, y);
-            cout << "    ";
-            GotoXY(x + 10, y);
-            cout << "                       ";
-            GotoXY(x - 4, ++y);
-            cout << "  [";
-            GotoXY(x + 10, y);
-            cout << "]  ← 스페이스바로 선택";
-            Sleep(100);
-        }
-        break;
-    case Key::SPACE:
-        PlaySoundID(SOUNDID::CLICK);
+	COORD resolution = GetConsoleResolution();
+	static const int originX = (resolution.X - 5) / 2 +20;
+	static int x = originX;
 
-        if (originY == y) return Menu::TITLE;
-        else if (originY + 2 == y) return Menu::QUIT;
-        break;
-    }
-    return Menu::FAIL;
+	int y = (resolution.Y - 11) / 2 + 9;
+	Key eKey = KeyController();
+	switch (eKey)
+	{
+	case Key::LEFT:
+		PlaySoundID(SOUNDID::UPDOWN);
+		if (x > originX - 20)
+		{
+			GotoXY(x + 3, y + 2);
+			cout << "    ";
+			GotoXY(x - 3, y + 4);
+			cout << "                 ";
+			GotoXY(x - 5, y);
+			cout << "    ";
+			GotoXY(x + 10, y);
+			cout << "    ";
+
+			x -= 40;
+
+			GotoXY(x - 4, y);
+			cout << "  [";
+			GotoXY(x + 9, y);
+			cout << " ]";
+			GotoXY(x + 4, y + 2);
+			cout << "↑";
+			GotoXY(x - 3, y + 4);
+			cout << "스페이스바로 선택";
+			Sleep(100);
+		}
+		break;
+	case Key::RIGHT:
+		PlaySoundID(SOUNDID::UPDOWN);
+		if (x < originX)
+		{
+			GotoXY(x + 3, y + 2);
+			cout << "    ";
+			GotoXY(x - 3, y + 4);
+			cout << "                 ";
+			GotoXY(x - 5, y);
+			cout << "    ";
+			GotoXY(x + 10, y);
+			cout << "    ";
+
+			x += 40;
+
+			GotoXY(x - 4, y);
+			cout << "  [";
+			GotoXY(x + 9, y);
+			cout << " ]";
+			GotoXY(x + 4, y + 2);
+			cout << "↑";
+			GotoXY(x - 3, y + 4);
+			cout << "스페이스바로 선택";
+			Sleep(100);
+		}
+		break;
+	case Key::SPACE:
+		PlaySoundID(SOUNDID::CLICK);
+
+		if (originX - 40 == x) return Menu::TITLE;
+		else if (originX == x) return Menu::QUIT;
+		break;
+	}
+	return Menu::FAIL;
 }
 
 void ResultScene::Update()
 {
+	if (Core::Instance->GetCurrentScene() != SCENE::END)
+		return;
 
+	Sleep(30);
+
+	Menu eMenu = GetCurMenu();
+	switch (eMenu)
+	{
+		case Menu::TITLE:
+		system("cls");
+		Core::Instance->ChangeScene(SCENE::TITLE);
+		break;
+	case Menu::QUIT:
+		Core::Instance->ChangeScene(SCENE::QUIT);
+		break;
+	}
 }
 
 void ResultScene::Render(int score)
 {
-    COORD resolution = GetConsoleResolution();
-    int x = (resolution.X / 2.1);
-    int y = (resolution.Y / 6) + 10;
-    GotoXY(x, y);
-    cout << "메인 메뉴";
-    GotoXY(x, y + 2);
-    cout << "게임 종료";
+	COORD resolution = GetConsoleResolution();
+	int originX = (resolution.X - 5) / 2;
+	int y = (resolution.Y - 11) / 2 + 9;
+
+	GotoXY(originX - 20, y);
+	cout << "메인 메뉴";
+
+	GotoXY(originX + 20, y);
+	cout << "게임 종료";
+
+	originX += 20;
+
+	GotoXY(originX - 4, y);
+	cout << "  [";
+	GotoXY(originX + 9, y);
+	cout << " ]";
+	GotoXY(originX + 4, y + 2);
+	cout << "↑";
+	GotoXY(originX - 3, y + 4);
+	cout << "스페이스바로 선택";
+
+
 }
 
 void ResultScene::RenderGameOver()
